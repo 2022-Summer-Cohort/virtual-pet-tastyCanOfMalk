30f343e18d29037cb48bbc85bd6a9baf76919c89
tastyCanOfMalk <eddytheflow@gmail.com>
2022-05-23 15:21:42 -0400

    add tick() method

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,2 +1,21 @@
-package virtual_pet;public class VirtualPet {
+package virtual_pet;
+
+public class VirtualPet {
+    private String name;
+    private int age;
+    private String type;
+
+    public VirtualPet(String name, int age, String type) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
+    }
+
+    public void tick(){
+        this.age++;
+    }
+
+    public String introduce(){
+        return("Say hello to " + this.name + " who is a " + this.age + " year old " + this.type);
+    }
 }
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -4,6 +4,17 @@ public class VirtualPetApplication {
 
     public static void main(String[] args) {
         //Interact with a VirtualPet object in this method
+
+        VirtualPet Paco = new VirtualPet("Paco", 2, "Alpaca");
+
+        System.out.println(Paco.introduce());
+        Paco.tick();
+        System.out.println(Paco.introduce());
+
+
+
+
+
     }
 
 }
